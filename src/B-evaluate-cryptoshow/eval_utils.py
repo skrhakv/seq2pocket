25ea2a91329e6ecbebe17315bf65195a26b742c7
Vít škrhák <v.skrhak@gmail.com>
2025-10-21 14:34:49 +0200

    run p2rank on non-cryptic pockets as well

--- src/B-evaluate-cryptoshow/eval_utils.py
+++ src/B-evaluate-cryptoshow/eval_utils.py
@@ -2,7 +2,7 @@ import csv
 
 DATA_PATH = '/home/skrhakv/cryptoshow-analysis/data/A-cluster-ligysis-data/clustered-binding-sites.txt'
 
-def read_test_binding_residues(data_path=DATA_PATH) -> set[int]:
+def read_test_binding_residues(data_path=DATA_PATH, pocket_types=['CRYPTIC']) -> set[int]:
     cryptic_binding_residues = {}
     sequences = {}
 
@@ -17,7 +17,7 @@ def read_test_binding_residues(data_path=DATA_PATH) -> set[int]:
                 continue
 
             binding_residue_indices = [f'{chain_id}_{int(i[1:])}'for i in row[3].split(' ')]
-            if row[2] == 'CRYPTIC':
+            if row[2] in pocket_types:
                 if protein_id not in cryptic_binding_residues:
                     cryptic_binding_residues[protein_id] = []
                 cryptic_binding_residues[protein_id].append(binding_residue_indices)
